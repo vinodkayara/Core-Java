0f5fd398bdd27068bf0ef37825fdfb714f97a728
Vinod Kayara <115912502+vinodkayara@users.noreply.github.com>
2025-08-02 22:26:00 +0530

    Add files via upload

---
+++ ErrorDemo.java
@@ -0,0 +1,19 @@
+package com.corejava;
+
+public class ErrorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+//			long a[]=new long[20000000000];
+			
+		}catch(Exception e){
+			System.out.println("catcj block executed");
+			
+			
+		}
+		System.out.println("program terminated normally");
+
+	}
+
+}
